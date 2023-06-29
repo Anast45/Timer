40a7a99eef43eff2dca688c858b306af90d14b87
Anastasios Dachis <48797123+AnastDachis@users.noreply.github.com>
2023-06-29 22:16:07 +0300

    Add files via upload
    The updated version

--- MainWindow.java
+++ MainWindow.java
@@ -1,133 +1,216 @@
-//Anastasios Dachis @2020// 
-//////////////////////////
-/////////////////////////
-package MainWindow;
-import java.awt.Color;
-import java.awt.FlowLayout;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timerupdated;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+public class MainWindow extends JFrame implements ActionListener {
 
-public class MainWindow extends JFrame implements ActionListener{
-		//Label
-		//JLabel userN = new JLabel("username :");
-		JLabel timeL = new JLabel("Δώσε ώρα που θα ήθελες να απενεργοποιηθεί ο υπολογιστής");
-		JLabel timeHL = new JLabel("Ώρα");
-		JLabel timeML = new JLabel("Λεπτά");
-		
-		//TextField
-		JTextField timerH = new JTextField(2);
-		JTextField timerM = new JTextField(2);
-		
-		//Button = "Κουμπιά"
-		JButton confirm = new JButton("Επιβεβαίωση");
-		JButton cb = new JButton("Αναίρεση");
-		JButton exit = new JButton("Exit");
-		
-		//Panel = "Ο Πίνακας : λευκός καμβας που μπαινουν τα αντικειμενα"
-		JPanel obj =new JPanel();
-				
-		//Flow = "Ροή Κειμένου Δεξιά-Αριστερά"
-		FlowLayout flow =new FlowLayout(FlowLayout.LEFT);
-		
-	public MainWindow(){
-		//Προσθήκη ActionListener και Χρωμάτων//
-		cb.addActionListener(this);
-		cb.setBackground(Color.ORANGE);
-		
-		confirm.addActionListener(this);
-		confirm.setBackground(Color.ORANGE);
+    private JComboBox<Integer> timerH;
+    private JComboBox<Integer> timerM;
+    private JButton confirm;
+    private JButton cb;
+    private JButton exit;
+    private JPanel obj;
+    private FlowLayout flow;
+    private boolean timerActive;
+    private Timer countdownTimer;
+    private JLabel countdownLabel;
+    private int remainingTime;
+    private boolean isGreek;
+    private JComboBox<String> languageBox;
 
-		exit.addActionListener(this);
-		exit.setBackground(Color.ORANGE);
-		
-		//Βάζοντας τα στοιχεία στο κύριο στοιχείο
-		obj.setBackground(Color.LIGHT_GRAY);
-		obj.add(timeL);
-		obj.add(Box.createHorizontalStrut(1000000));
-		obj.add(timeHL);
-		obj.add(timerH);
-		obj.add(timeML);
-		obj.add(timerM);
-		obj.add(confirm);
-		obj.add(Box.createHorizontalStrut(1000000));
-		obj.add(cb);
-		obj.add(exit);
-		
-		//The area where components are inserted
-		setContentPane(obj);
-	
-	}
-	
-	public void actionPerformed(ActionEvent evt) {
-		Object source=evt.getSource();
-		if(source == exit )
-			exitForm();
-		else if(source == confirm) {
-				String answer = "Λεπτά για να κλείσει ο Η/Υ σας  "+timerH.getText()+" : "+timerM.getText();
-				
-				try {
-					shutdown(timerH.getText(),timerM.getText());
-				} 
-				catch (RuntimeException | IOException e) {
-					e.printStackTrace();
-				}
-				
-				JOptionPane.showMessageDialog(null,answer,"Application", JOptionPane.WARNING_MESSAGE);
-			}
-		else if(source == cb) {
-			try {
-				antishutdown();
-			} catch (RuntimeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			JOptionPane.showMessageDialog(null,"Your timer has been canceled","Application", JOptionPane.WARNING_MESSAGE);
-			}
-		}
-	
-	private void exitForm() {
-		dispose();
-	}
-	
-	public void shutdown(String H , String M) throws RuntimeException, IOException {
-	    String shutdownCommand;
-	    int K=Integer.parseInt(H);
-	    int J=Integer.parseInt(M);
-	    K=K*3600+J*60;
-	    
-	    shutdownCommand = "shutdown.exe -s -t "+K;
-	    Runtime.getRuntime().exec(shutdownCommand);
-	    //System.exit(0);
-	}
-	
-	public static void antishutdown() throws RuntimeException, IOException {
-	    String shutdownCommand;
-	    shutdownCommand = "shutdown.exe -a";
-	    
-	    Runtime.getRuntime().exec(shutdownCommand);
-	    //System.exit(0);
-	}
-	
-	    public static void main(String[] args) {
-		MainWindow form= new MainWindow();
-		
-		form.setSize(500,200);
-		form.setTitle("Timer-Διακόπτης");
-		form.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		form.setLocation(670, 500);
-		
-		form.show();
-	}
+    public MainWindow() {
+        timerActive = false;
+        setTitle("Timer-Shutdown");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 250);
+        setLocationRelativeTo(null);
+        initComponents();
+        setContentPane(obj);
+        countdownTimer = new Timer(1000, this);
+    }
 
+    private void initComponents() {
+        // Labels
+        JLabel timeL = new JLabel("Enter the time at which you want to shut down your computer");
+        JLabel timeHL = new JLabel("Hours");
+        JLabel timeML = new JLabel("Minutes");
+
+        // ComboBoxes for hours and minutes
+        timerH = new JComboBox<>();
+        timerM = new JComboBox<>();
+
+        for (int i = 0; i <= 10; i++) {
+            timerH.addItem(i);
+        }
+
+        for (int i = 0; i <= 60; i++) {
+            timerM.addItem(i);
+        }
+
+        // Buttons
+        confirm = new JButton("Confirm");
+        cb = new JButton("Cancel");
+        exit = new JButton("Exit");
+
+        // Language selection box
+        languageBox = new JComboBox<>();
+        languageBox.addItem("English");
+        languageBox.addItem("Greek");
+        languageBox.addActionListener(this);
+
+        // Panel
+        obj = new JPanel();
+        flow = new FlowLayout(FlowLayout.LEFT);
+        obj.setLayout(flow);
+        obj.setBackground(Color.LIGHT_GRAY);
+
+        // Add components to the panel
+        obj.add(languageBox);
+        obj.add(Box.createRigidArea(new Dimension(20, 0)));
+        obj.add(timeL);
+        obj.add(timeHL);
+        obj.add(timerH);
+        obj.add(timeML);
+        obj.add(timerM);
+        obj.add(confirm);
+        obj.add(cb);
+        obj.add(exit);
+
+        // Register action listeners
+        confirm.addActionListener(this);
+        cb.addActionListener(this);
+        exit.addActionListener(this);
+
+        // Countdown label
+        countdownLabel = new JLabel();
+        countdownLabel.setFont(new Font("Arial", Font.BOLD, 24));
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        Object source = evt.getSource();
+
+        if (source == confirm) {
+            if (!timerActive) {
+                int hours = (int) timerH.getSelectedItem();
+                int minutes = (int) timerM.getSelectedItem();
+                remainingTime = hours * 3600 + minutes * 60;
+                if (remainingTime > 0) {
+                    showShutdownTime(hours, minutes);
+                }
+                startCountdown();
+                showTimerActivatedPopup();
+            }
+        } else if (source == cb) {
+            if (timerActive) {
+                stopCountdown();
+                showTimerDeactivatedPopup();
+            }
+        } else if (source == countdownTimer) {
+            remainingTime--;
+            if (remainingTime <= 0) {
+                stopCountdown();
+                showShutdownMessage();
+            } else {
+                countdownLabel.setText(formatTime(remainingTime));
+            }
+        } else if (source == exit) {
+            int confirmed = JOptionPane.showConfirmDialog(null,
+                    "Are you sure you want to exit the program?", "Exit Confirmation",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+            if (confirmed == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        } else if (source == languageBox) {
+            String selectedLanguage = (String) languageBox.getSelectedItem();
+            if (selectedLanguage.equals("Greek")) {
+                setLanguage("Greek");
+            } else {
+                setLanguage("English");
+            }
+        }
+    }
+
+    private void showShutdownTime(int hours, int minutes) {
+        String shutdownTime = formatTime(hours) + ":" + formatTime(minutes);
+        JOptionPane.showMessageDialog(this, "The computer will shut down at " + shutdownTime,
+                "Timer Activated", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void showTimerActivatedPopup() {
+        JOptionPane.showMessageDialog(this, "Timer activated. Computer will shut down in " +
+                formatTime(remainingTime), "Timer Activated", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void showTimerDeactivatedPopup() {
+        JOptionPane.showMessageDialog(this, "Timer deactivated.", "Timer Deactivated",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void showShutdownMessage() {
+        JOptionPane.showMessageDialog(this, "Shutdown time reached. The computer will now shut down.",
+                "Shutdown", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void startCountdown() {
+        countdownTimer.start();
+        timerActive = true;
+        confirm.setEnabled(false);
+        timerH.setEnabled(false);
+        timerM.setEnabled(false);
+        obj.add(countdownLabel);
+        obj.revalidate();
+        obj.repaint();
+    }
+
+    private void stopCountdown() {
+        countdownTimer.stop();
+        timerActive = false;
+        confirm.setEnabled(true);
+        timerH.setEnabled(true);
+        timerM.setEnabled(true);
+        obj.remove(countdownLabel);
+        obj.revalidate();
+        obj.repaint();
+    }
+
+    private String formatTime(int time) {
+        return String.format("%02d", time);
+    }
+
+    private void setLanguage(String language) {
+        if (language.equals("Greek")) {
+            setTitle("Χρονομετρητής-Απενεργοποίηση");
+            JLabel timeL = new JLabel("Εισάγετε την ώρα κλεισίματος του υπολογιστή σας");
+            JLabel timeHL = new JLabel("Ώρες");
+            JLabel timeML = new JLabel("Λεπτά");
+            confirm.setText("Επιβεβαίωση");
+            cb.setText("Ακύρωση");
+            exit.setText("Έξοδος");
+            languageBox.setSelectedItem("Ελληνικά");
+        } else {
+            setTitle("Timer-Shutdown");
+            JLabel timeL = new JLabel("Enter the time at which you want to shut down your computer");
+            JLabel timeHL = new JLabel("Hours");
+            JLabel timeML = new JLabel("Minutes");
+            confirm.setText("Confirm");
+            cb.setText("Cancel");
+            exit.setText("Exit");
+            languageBox.setSelectedItem("English");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainWindow form = new MainWindow();
+            form.setVisible(true);
+        });
+    }
 }
-
